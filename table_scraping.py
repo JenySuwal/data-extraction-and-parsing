758451dd80800d68b37f617a885603cee5251ff0
unknown <jenisasuwal@gmail.com>
2025-04-24 17:49:41 +0545

    updated path

--- table_scraping.py
+++ table_scraping.py
@@ -120,8 +120,8 @@ def fetch_tables_html(url, crawl_id):
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--remote-debugging-port=9222")
-        # chrome_options.binary_location = "/usr/bin/google-chrome"
-        chrome_options.binary_location = "/snap/bin/chromium"
+        chrome_options.binary_location = "/usr/bin/google-chrome"
+        # chrome_options.binary_location = "/snap/bin/chromium"
         # chromedriver_path = ChromeDriverManager(version="135.0.7049.95").install()
         # chromedriver_path = ChromeDriverManager(version="135.0.7049.95").install()
 
